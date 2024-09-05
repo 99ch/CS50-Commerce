aedb172e61f737904c098d9512b8370f3702274f
99ch <[chilavertndah99@gmail.com]>
2024-09-05 01:05:42 +0100

    Creation des modeles Listing et Category; defintion de la fonction createListing

---
+++ auctions/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import User,Category,Listing
+
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Listing)
+# Register your models here.
